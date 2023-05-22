1d5c8361151a79157de8f2e89a5ae0c061dec981
amp-m9 <andrickposo3001@gmail.com>
2023-05-22 23:10:50 +0100

    clean-up

--- src/main/java/xyz/andrick/figures/FiguresApplication.java
+++ src/main/java/xyz/andrick/figures/FiguresApplication.java
@@ -4,10 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,19 +13,10 @@ public class FiguresApplication extends Application {
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FiguresApplication.class.getResource("session-settings-view.fxml"));
         Parent root = fxmlLoader.load();
-        AnchorPane settings = new AnchorPane();
-
-//        String css = getClass().getResource("session-settings.css").toExternalForm();
-//        scene.getStylesheets().add(css);
-
         Scene scene = new Scene(root);
-
         primaryStage.setTitle("Figures: Create a session");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    private void makeSettings(){
     }
 
     public static void main(String[] args) {
