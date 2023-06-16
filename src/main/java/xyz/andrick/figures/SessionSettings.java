b4c6d5ef77f353fda84864538eb8edbd30cd75fa
amp-m9 <andrickposo3001@gmail.com>
2023-06-17 00:28:27 +0100

    Added option to shuffle and limit images.

--- src/main/java/xyz/andrick/figures/SessionSettings.java
+++ src/main/java/xyz/andrick/figures/SessionSettings.java
@@ -3,4 +3,11 @@ package xyz.andrick.figures;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public record SessionSettings(long imageTimeMillis, long breakTimeMillis, String directory, Integer picturesBetweenBreaks, String[] imageSources, Scene previousScene, Stage stage) {}
+public record SessionSettings(long imageTimeMillis,
+                              long breakTimeMillis,
+                              String directory,
+                              Integer picturesBetweenBreaks,
+                              String[] imageSources,
+                              Scene previousScene,
+                              Stage stage) {
+}
