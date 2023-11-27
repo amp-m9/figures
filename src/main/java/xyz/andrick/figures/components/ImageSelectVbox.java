67bcce1f9512950c844d57c3c413d75168c74965
Andrick <andrickposo3001@gmail.com>
2023-11-27 21:07:05 +0000

    grow image to fill box height

--- src/main/java/xyz/andrick/figures/components/ImageSelectVbox.java
+++ src/main/java/xyz/andrick/figures/components/ImageSelectVbox.java
@@ -4,8 +4,11 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import xyz.andrick.figures.records.PexelImage;
 
@@ -14,6 +17,12 @@ import java.io.IOException;
 import static xyz.andrick.figures.utilities.Helpers.getInputStreamFromUrl;
 
 public class ImageSelectVbox extends VBox {
+    boolean selected = false;
+    ImageView imageView;
+    BorderPane imagePane;
+    Label label;
+    int fontSize = 15;
+
     public ImageSelectVbox(PexelImage photo) {
         super();
         try {
@@ -21,21 +30,29 @@ public class ImageSelectVbox extends VBox {
             if (image.isError()) {
                 throw new RuntimeException("Cannot load image");
             }
-            ImageView imageView = new ImageView(image);
+            imageView = new ImageView(image);
             imageView.setPreserveRatio(true);
 
-            BorderPane imagePane = new BorderPane();
+            imagePane = new BorderPane();
             imagePane.setCenter(imageView);
 
-            Label label = new Label(photo.alt());
+            label = new Label(photo.alt());
             label.setAlignment(Pos.TOP_CENTER);
             label.setTextAlignment(TextAlignment.CENTER);
             label.prefWidthProperty().bind(widthProperty());
+            label.fontProperty().set(Font.font(fontSize));
+            label.setPrefHeight(computeMinHeight(1));
             getChildren().add(imagePane);
             getChildren().add(label);
+            setVgrow(imagePane, Priority.ALWAYS);
+            setOnMouseClicked(this::onMouseClicked);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private void onMouseClicked(MouseEvent mouseEvent) {
+        selected = !selected;
     }
 }
